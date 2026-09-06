51418140b887c4bc75ca31f633571cb6dd5911b0
bdutt001 <bdutt001@odu.edu>
2026-09-06 13:38:25 -0400

    added deploy script and reconfigured

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,51 @@ function App() {
   return (
     <div>
       <nav className="navbar">
-        <h1>Benjamin Dutton</h1>
-
+        <h1></h1>
         <div>
-          <a href="#work">Work</a>
+          <a href="#work">Gallery</a>
           <a href="#about">About</a>
           <a href="#contact">Contact</a>
         </div>
       </nav>
 
-      <main>
-        <section className="hero">
+      <div className="hero">
           <img
-            src="/photos/landscape/sandpiper-001.jpeg"
+            src={`${import.meta.env.BASE_URL}/photos/landscape/sandpiper-001.jpeg`}
             alt="Featured photography"
           />
+      </div>
 
+      <main>
+        <section className="hero-content">
           <div className="hero-text">
-            <h2>Photography Portfolio</h2>
+            <h2>Photography <br/> Portfolio</h2>
+            <h3>Benjamin Dutton</h3>
           </div>
         </section>
 
-        <section id="work" className="work">
-          <p>SELECTED WORK</p>
-          <h2>Recent photographs</h2>
-
-          <div className="gallery">
-            <img src="/photos/landscape/black-widow-001.jpeg" alt="Southern Black Widow" />
-            <img src="/photos/landscape/crepe-myrtle.jpeg" alt="Crepe Myrtle" />
-            <img src="/photos/landscape/egret-001.jpeg" alt="Great Egret" />
-          </div>
-        </section>
-
-        <section id="about" className="about">
-          <h2>About</h2>
-          <p>
-            I'm a photographer
-          </p>
-        </section>
-
-        <section id="contact" className="contact">
-          <h2>Let's work together.</h2>
-          <a href="mailto:hello@example.com">Get in touch</a>
-        </section>
+        <div className='glass'>
+          <section id="work" className="work">
+            <p>Gallery</p>
+            <h2>Recent photographs</h2>
+            <div className="gallery">
+              <img src={`${import.meta.env.BASE_URL}/photos/landscape/egret-001.jpeg`} alt="Great Egret" />
+              <img src={`${import.meta.env.BASE_URL}/photos/landscape/black-widow-001.jpeg`} alt="Southern Black Widow" />
+              <img src={`${import.meta.env.BASE_URL}/photos/landscape/crepe-myrtle-001.jpeg`} alt="Crepe Myrtle" />
+              
+            </div>
+          </section>
+          <section id="about" className="about">
+            <h2>About</h2>
+            <p>
+              I'm a photographer
+            </p>
+          </section>
+          <section id="contact" className="contact">
+            <h2>Let's work together.</h2>
+            <a href="mailto:bendutton9@gmail.com">Get in touch</a>
+          </section>
+        </div>
       </main>
     </div>
   );
